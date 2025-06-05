6de9a3c387eede79d5052278e1b3151340923020
Dimas Aparecido Rabelo de Souza <146005383+DimasRabelo@users.noreply.github.com>
2025-06-05 07:19:42 -0300

    Initial commit

---
+++ src/main/java/me/dio/dev_cloud_bradesco/DevCloudBradescoApplication.java
@@ -0,0 +1,13 @@
+package me.dio.dev_cloud_bradesco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevCloudBradescoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevCloudBradescoApplication.class, args);
+	}
+
+}
